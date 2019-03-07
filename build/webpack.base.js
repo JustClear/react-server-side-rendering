59a909776f04a4f617c96670dc7ea07dd2f92993
大板栗 <576839306@qq.com>
2019-03-07 13:41:13 +0800

    📝 update configure

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -7,13 +7,14 @@ module.exports = {
     resolve: {
         extensions: ['.js', '.jsx', '.json'],
         alias: {
-            '@src': path.resolve(__dirname, `src`),
-            '@pages': path.resolve(__dirname, `src/pages`),
-            '@store': path.resolve(__dirname, `src/store`),
-            '@common': path.resolve(__dirname, `src/common`),
-            '@actions': path.resolve(__dirname, `src/actions`),
-            '@reducers': path.resolve(__dirname, `src/reducers`),
-            '@components': path.resolve(__dirname, `src/components`),
+            '@src': path.resolve(__dirname, `../src`),
+            '@pages': path.resolve(__dirname, `../src/pages`),
+            '@store': path.resolve(__dirname, `../src/store`),
+            '@routes': path.resolve(__dirname, `../src/routes`),
+            '@common': path.resolve(__dirname, `../src/common`),
+            '@actions': path.resolve(__dirname, `../src/actions`),
+            '@reducers': path.resolve(__dirname, `../src/reducers`),
+            '@components': path.resolve(__dirname, `../src/components`),
         },
     },
     module: {
